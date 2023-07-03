4d84270669f1edbba52e8d9422c04cc644345f8c
Fernando Lucas <fernandolcsdev@outlook.com>
2023-07-03 15:47:50 -0300

    Método __call()

--- metodos-magicos/index.php
+++ metodos-magicos/index.php
@@ -11,4 +11,22 @@
     $obj = new C();// 2 (__construct)
     echo $obj(); // 3 (__invoke)
 
+    class Test {
+        public function __call($name, $args){
+            call_user_func_array(array('static', "test$name"), $args);
+        }
+
+        public function testS($l) {
+            echo "$l,";
+        }
+    }
+
+    class Test2 extends Test {
+        public function testS($l) {
+            echo "$l,$l,";
+        }
+    }
+    
+    $test = new Test2();
+    $test->S('A');// A,A => S não existe é chamado __call()
 ?>
