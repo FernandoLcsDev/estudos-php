bec32e30253688a114f740413c34aaafd12b055f
Fernando Lucas <fernandolcsdev@outlook.com>
2023-07-11 16:33:23 -0300

    Lógica de programação

--- logica/index.php
+++ logica/index.php
@@ -1,6 +1,8 @@
 <?php 
 
-    echo '1' . (print '2') + 3; // 214
+    echo '1' . (print '2') + 3; // Ordem: (print '2') -> 2 | echo (print '2') -> 1 + 4 ==> 4 | Resultado: 214
+
+    echo '</br>';
 
     $a = 3;
     switch ($a) {
@@ -10,13 +12,30 @@
         case 3: echo 'three'; break;
     }//three
 
+    echo '</br>';
+
     $a = 'a'; $b = 'b';
     echo isset($c) ? $a.$b.$c : ($c = 'c').'d'; // cd
 
+    echo '</br>';
+
     echo "22" + "0.2", 23 . 1;// 22 + 0.2 = 22.2 | 23 . 1 = 231 | 22.2 . 231 => 22.2231
+
+    echo '</br>';
 
     $first = "second";
     $second = "first";
     echo $$$first; // second
 
+    echo '</br>';
+
+    function z($x) {
+        return function ($y) use ($x) {
+            return str_repeat($y, $x);
+        };
+    }
+    
+    $a = z(2);
+    $b = z(3);
+    echo $a(3) . $b(5);
 ?>
